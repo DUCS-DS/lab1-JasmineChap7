1ff85f5fee961a33ed5a1bf4ea060fcd1abab4e1
JasmineChap7 <jgray014@drury.edu>
2025-02-17 12:23:45 -0600

    final lab 1 code

--- edit_me.py
+++ edit_me.py
@@ -1,6 +1,10 @@
-def FindMax(inputArray):
+def find_max(inputArray):
     max_value = inputArray[0]  
     for i in range(1, len(inputArray)):  
         if inputArray[i] > max_value:  
             max_value = inputArray[i] 
     return max_value  #return the maximum
+
+test_list = [2112*i % 2024 for i in range(10000)]
+
+print(find_max(test_list))
